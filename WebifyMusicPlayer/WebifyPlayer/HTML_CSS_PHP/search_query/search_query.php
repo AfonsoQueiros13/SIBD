5c723158724da006f7cd3254a195784cf5ecc2c0
BrunoBaptista-1 <baptistabruno5@gmail.com>
2019-12-12 23:51:06 +0000

    searchquery working

--- WebifyMusicPlayer/WebifyPlayer/HTML_CSS_PHP/search_query/search_query.php
+++ WebifyMusicPlayer/WebifyPlayer/HTML_CSS_PHP/search_query/search_query.php
@@ -101,15 +101,16 @@
 
 
       $count = 0;
+      $count2=0;
       foreach ($all_albums as $album) {
         $path = get_album_image($album['nome_album']);
         //$album = get_album_by_id($all_albums[$count++]['nome_album']);
-        $album = $all_albums[$count]['nome_album'];
+        $album = $all_albums[$count++]['nome_album'];
         ?>
 
         <li>
           <a href="../artist-guest/artist-guest.php?id=<?= $album['id'] ?>">
-            <img src="<?=$path[$count]['img_path']?>" alt="artistcover">
+            <img src="<?=$path[$count2]['img_path']?>" alt="artistcover">
             <div>
               <?= $album ?>
             </div>
